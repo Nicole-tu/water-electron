2932adb88d87e5a084100354e7098305f871124b
Nicole Tu 杜晨寧 <1801665@C0699.local>
2020-08-18 17:00:17 +0800

    add build

--- main.js
+++ main.js
@@ -16,28 +16,30 @@ function createWindow() {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// 有些 API 只能在這個事件發生後才能用。
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // 在 macOS 中，一般會在使用者按了 Dock 圖示
+    // 且沒有其他視窗開啟的情況下，
+    // 重新在應用程式裡建立視窗。
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// 在 macOS 中，一般會讓應用程式及選單列繼續留著，
+// 除非使用者按了 Cmd + Q 確定終止它們
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+// 你也可以將它們放在別的檔案裡，再由這裡 require 進來。
