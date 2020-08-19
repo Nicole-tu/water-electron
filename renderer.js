ddef1d62702973f7fd5b4b41b695aadad1bed395
Nicole Tu 杜晨寧 <1801665@C0699.local>
2020-08-19 16:35:36 +0800

    add css

--- renderer.js
+++ renderer.js
@@ -4,3 +4,29 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const ageElm = document.getElementById('age')
+const weightElm = document.getElementById('weight')
+const totalAmountElm = document.getElementById('totalAmount')
+const waterElm = document.getElementsByClassName('water')
+
+
+const calculateAmount = () => {
+
+  if (ageElm.value.trim() == '' || weightElm.value.trim() == '') {
+    alert('Please input data');
+    return;
+  }
+  if (ageElm.value < 30) {
+    totalAmountElm.value = weightElm.value * 40
+  } else if (ageElm.value >= 30 && ageElm.value <= 55) {
+    totalAmountElm.value = weightElm.value * 35
+  } else if (ageElm.value > 55) {
+    totalAmountElm.value = weightElm.value * 30
+  }
+}
+
+const addWater = () => {
+  waterElm[0].classList.add('water-full');
+  waterElm[0].classList.add('active');
+}
